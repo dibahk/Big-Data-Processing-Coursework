d93d20eafa862f5fb824db3fcc84f2b980a2bdf5
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-12 20:33:17 +0000

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,45 @@
+import sys, string
+import os
+import socket
+import time
+import operator
+import boto3
+import json
+from pyspark.sql import SparkSession
+from datetime import datetime
+from pyspark.sql.functions import from_unixtime, date_format
+from pyspark.sql.functions import to_date, count, col
+from graphframes import *
+
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession\
+        .builder\
+        .appName("TestDataset")\
+        .getOrCreate()
+    
+    # shared read-only object bucket containing datasets
+    s3_data_repository_bucket = os.environ['DATA_REPOSITORY_BUCKET']
+
+    s3_endpoint_url = os.environ['S3_ENDPOINT_URL']+':'+os.environ['BUCKET_PORT']
+    s3_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    s3_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    s3_bucket = os.environ['BUCKET_NAME']
+
+    hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
+    hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
+    hadoopConf.set("fs.s3a.secret.key", s3_secret_access_key)
+    hadoopConf.set("fs.s3a.path.style.access", "true")
+    hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
+
+    rideshare_data = spark.sparkContext.textFile("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/sample_data.csv") # /read from the bucket file
+    taxi_zone_lookup_df = spark.sparkContext.textFile("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/taxi_zone_lookup.csv") # /read from the bucket file
+    print(taxi_zone_lookup_df)
+
+
+
+    
+    spark.stop()
