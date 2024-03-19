4033f200e353b867feb59fef61ffe588e429ffc1
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-19 16:55:48 +0000

    Update task4.py

--- task4.py
+++ task4.py
@@ -63,15 +63,43 @@ if __name__ == "__main__":
                                                                                                          
                                                                                             
     # TASK 4
-    # 1
     # turning the dataframe file to rdd
     df = join_df.rdd
+    # 1
+    # extracting time of the day and driver_total_pay because these are the columns that we want to analyse
     data_1 = df.map(lambda x: (x[8], float(x[11])))
+    # since we need the average I am also going to employ another map on the values precisely to also add a column of 1 as a way to count the values in each key
     data_1 = data_1.mapValues(lambda x:(x, 1))
+    # reducing the RDD by summing the driver_total_pay and counting instances of each key
     data_1 = data_1.reduceByKey(lambda x,y :(x[0]+y[0],x[1]+y[1]))
+    # divinding the summation of driver_total_pay and counts of each key to calculate average
     averages_1 = data_1.mapValues(lambda x : x[0]/x[1])
+    # creating the dataframe with the required column names
     df_1 = spark.createDataFrame(averages_1, ["time_of_day", "average_drive_total_pay"])
+    # showing the dataframe in the terminal and sorting it as required
     df_1.sort(desc("average_drive_total_pay")).show(df_1.count(), truncate = False)
+
+    # 2
+    # extracting time of the day and trip_length because these are the columns that we want to analyse
+    data_2 = df.map(lambda x: (x[8], float(x[3])))
+    # since we need the average I am also going to employ another map on the values precisely to also add a column of 1 as a way to count the values in each key
+    data_2 = data_2.mapValues(lambda x:(x, 1))
+    # reducing the RDD by summing the trip_length and counting instances of each key
+    data_2 = data_2.reduceByKey(lambda x,y :(x[0]+y[0],x[1]+y[1]))
+    # divinding the summation of trip_length and counts of each key to calculate average
+    averages_2 = data_2.mapValues(lambda x : x[0]/x[1])
+    # creating the dataframe with the required column names
+    df_2 = spark.createDataFrame(averages_2, ["time_of_day_2", "average_trip_length"])
+    # showing the dataframe in the terminal and sorting it as required
+    df_2.sort(desc("average_trip_length")).show(df_2.count(), truncate = False)
+
+    # 3
+    df_3 = df_1.join(df_2, df_1.time_of_day == df_2.time_of_day_2, 'inner')
+    df_3 = df_3.drop('time_of_day_2')
+    df_3 = df_3.withColumn('average_earning_per_mile', df_3['average_drive_total_pay'] / df_3['average_trip_length'])
+    df_3 = df_3.drop('average_drive_total_pay')
+    df_3 = df_3.drop('average_trip_length')
+    df_3.show()
     
     my_bucket_resource = boto3.resource('s3',
             endpoint_url='http://' + s3_endpoint_url,
@@ -82,8 +110,8 @@ if __name__ == "__main__":
     my_result_object = my_bucket_resource.Object(s3_bucket,'Result/taskThree_1.txt')
     my_result_object.put(Body=json.dumps(averages_1.collect()))
 
-    # my_result_object = my_bucket_resource.Object(s3_bucket,'Result/taskThree_2.txt')
-    # my_result_object.put(Body=json.dumps(data_2.collect()))
+    my_result_object = my_bucket_resource.Object(s3_bucket,'Result/taskThree_2.txt')
+    my_result_object.put(Body=json.dumps(data_2.collect()))
 
     # my_result_object = my_bucket_resource.Object(s3_bucket,'Result/taskThree_3.txt')
     # my_result_object.put(Body=json.dumps(data_3.collect()))
