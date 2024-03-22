4372ab67661089e279bdb8d5e3ff06bbfdff4f1c
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-22 23:10:14 +0000

    Finishing

--- task3.py
+++ task3.py
@@ -6,7 +6,7 @@ import operator
 import boto3
 import json
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql.functions import desc, asc
 from datetime import datetime
 from pyspark.sql.functions import from_unixtime, date_format
 from pyspark.sql.functions import to_date, count, col
@@ -37,8 +37,8 @@ if __name__ == "__main__":
     # TASK 1
     # 1
     # Loading data
-    rideshare_df = spark.read.csv("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/sample_data.csv",header=True)
-    # rideshare_df = spark.read.csv("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/rideshare_data.csv",header=True)
+    # rideshare_df = spark.read.csv("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/sample_data.csv",header=True)
+    rideshare_df = spark.read.csv("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/rideshare_data.csv",header=True)
     taxi_zone_df = spark.read.csv("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/taxi_zone_lookup.csv", header=True)
     
     # 2
@@ -60,39 +60,34 @@ if __name__ == "__main__":
     # join_df = join_df.withColumn(unix_timestamp("timestamp_col"), "yyyy-MM-dd").cast("date")
     join_df = join_df.withColumn("date", from_unixtime(col("date")).cast("date"))
     join_df = join_df.withColumn("date", date_format(col("date"), "yyyy-MM-dd"))
-                      
-    # join_df.show()                                                                                                                         
-
-    # 4                                                                                                        
-    # printing the number of rows
-    row_count = join_df.count()
-    print(f'The DataFrame has {row_count} rows.')                                                                                      
+                                                                                          
                                                                                             
     # TASK 3
     # 1
     # turning the dataframe file to rdd
     df = join_df.rdd
-    # function for defining the schema because the key is like [[borough, month], count] and we want it to be [borough, month, count]
+    function for defining the schema because the key is like [[borough, month], count] and we want it to be [borough, month, count]
     def explode_key(key_value):
         key, value = key_value
-        print(key)
-        print(value)
         return [key[0], key[1], value]
 
-    # mapping data to find the borough and the month
+    mapping data to find the borough and the month
     data_1 = df.map(lambda x: ((x[15], x[9][5:7]), 1))
     # reducing to find the number of occurences of the pair
-    data_1 = data_1.reduceByKey(add).sortBy(lambda x: x[1], ascending = False)
-    
+    data_1 = data_1.reduceByKey(add)
+    # we only need the first 5 rows
     # Apply the explode_key function to each element of the RDD using flatMap
     exploded_rdd = data_1.map(explode_key)
     df_1 = spark.createDataFrame(exploded_rdd, ["Pickup_Borough", "Month", "trip_count"])
-    df_1.show() 
+    df_1.sort(asc("Month"), desc("trip_count")).show(5, truncate = False)
+    data_1 = df_1.rdd
 
     # 2
     data_2 = df.map(lambda x: ((x[18], x[9][5:7]), 1))
     # reducing to find the number of occurences of the pair
-    data_2 = data_2.reduceByKey(add).sortBy(lambda x: x[1], ascending = False)
+    data_2 = data_2.reduceByKey(add)
+    
+    # function for defining the schema because the key is like [[borough, month], count] and we want it to be [borough, month, count]
     def explode_key(key_value):
         key, value = key_value
         return [key[0], key[1], value]
@@ -101,7 +96,7 @@ if __name__ == "__main__":
     exploded_rdd = data_2.map(explode_key)
     # Defining columns of the data frame
     df_2 = spark.createDataFrame(exploded_rdd, ["Dropoff_Borough", "Month", "trip_count"])
-    df_2.show() 
+    df_2.sort(asc("Month"), desc("trip_count")).show(5, truncate= False) 
 
     # 3
     # defining the schema of the dataframe
@@ -116,7 +111,7 @@ if __name__ == "__main__":
     # Apply the route function to each element of the RDD using flatMap
     exploded_rdd = data_3.map(route)
     df_3 = spark.createDataFrame(exploded_rdd, ["Route", "total_profit"])
-    df_3.show() 
+    df_3.show(30, truncate= False) 
     
     my_bucket_resource = boto3.resource('s3',
             endpoint_url='http://' + s3_endpoint_url,
@@ -125,7 +120,7 @@ if __name__ == "__main__":
                  
                            
     my_result_object = my_bucket_resource.Object(s3_bucket,'Result/taskThree_1.txt')
-    my_result_object.put(Body=json.dumps(df_1.collect()))
+    my_result_object.put(Body=json.dumps(data_1.collect()))
 
     my_result_object = my_bucket_resource.Object(s3_bucket,'Result/taskThree_2.txt')
     my_result_object.put(Body=json.dumps(data_2.collect()))
