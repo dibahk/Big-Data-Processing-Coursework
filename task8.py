19988a9cb9862f7644f2ae609221adbc149664cc
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-04-02 09:16:44 +0100

    Create task8.py

---
+++ task8.py
@@ -0,0 +1,59 @@
+import sys, string
+import os
+import socket
+import time
+import operator
+import boto3
+import json
+from pyspark.sql import SparkSession
+from datetime import datetime
+
+from functools import reduce
+from pyspark.sql.functions import col, lit, when
+from pyspark import *
+from pyspark.sql import *
+from pyspark.sql.types import *
+import graphframes
+from graphframes import *
+
+if __name__ == "__main__":
+
+    spark = SparkSession\
+        .builder\
+        .config("spark.jars.packages", "graphframes:graphframes:0.8.2-spark3.2-s_2.12")\
+        .appName("graphframes")\
+        .getOrCreate()
+
+    sqlContext = SQLContext(spark)
+    s3_data_repository_bucket = os.environ['DATA_REPOSITORY_BUCKET']
+
+    s3_endpoint_url = os.environ['S3_ENDPOINT_URL']+':'+os.environ['BUCKET_PORT']
+    s3_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    s3_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    s3_bucket = os.environ['BUCKET_NAME']
+
+    hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
+    hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
+    hadoopConf.set("fs.s3a.secret.key", s3_secret_access_key)
+    hadoopConf.set("fs.s3a.path.style.access", "true")
+    hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
+
+    # GraphFrames will expect to have our key named as 'id'
+    vertexSchema = StructType([StructField("id", IntegerType(), False),
+                               StructField("birthday", IntegerType(), True),
+                               StructField("hometown_id", IntegerType(), True),
+                               StructField("work_employer_id", IntegerType(), True),
+                               StructField("education_school_id", IntegerType(), True),
+                               StructField("education_year_id", IntegerType(), True)])
+
+    edgeSchema = StructType([StructField("src", IntegerType(), False),
+                               StructField("dst", IntegerType(), False)])
+
+    # reading from dataset to load edges and vertices data respectivily
+    edgesDF = spark.read.format("csv").options(header='True').schema(edgeSchema).csv("s3a://" + s3_data_repository_bucket + "/ECS765/graph/stanford_fb_edges.csv")
+    verticesDF = spark.read.format("csv").options(header='True').schema(vertexSchema).csv("s3a://" + s3_data_repository_bucket + "/ECS765/graph/stanford_fb_vertices.csv")
+
+    # showing 10 rows from the vertices and edges tables
+    verticesDF.show(10)
+    edgesDF.show(10)
